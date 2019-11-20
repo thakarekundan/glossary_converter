7a1ddf98929364a6024f3f0f2e289149f8ce7af9
Thakare Kundan <44129166+thakarekundan@users.noreply.github.com>
2019-11-20 10:37:48 +0530

    Delete views.py

--- views.py
+++
@@ -1,6 +0,0 @@
-import textconversion
-import dictionaryconversion
-import json_download
-textfile=textconversion.pdf_to_text('se.pdf')
-dictionary=dictionaryconversion.dictionary_convertor(textfile.get_text())
-s=json_download.json_downloaded(dictionary)
